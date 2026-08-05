d3527f816015c8424ea2527913247ff4f1ce3a66
phelipe-viana <phelipegabriel1988@gmail.com>
2026-08-05 15:22:57 -0400

    feat: update main menu choices in model initialization

--- main.go
+++ main.go
@@ -109,7 +109,7 @@ func main() {
 	m := model{
 		state:             stateMainMenu,
 		cursor:            0,
-		choices:           []string{"Nao Importa ess..", "Sair (Exit)"},
+		choices:           []string{"PRIMEIRO MENU", "Sair (Exit)"},
 		migrationsChoices: []string{"Criar nova Migration", "Executar Migrations pendentes", "Reverter última Migration (Rollback)", "Voltar ao menu principal"},
 	}
 
